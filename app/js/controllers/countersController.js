ff50733f5cca0212783e27373386021cfd6c5d90
CecileCee <cecile.muloway@gmail.com>
2016-04-18 10:59:14 +0200

    création d'un champ de saisie de texte

--- app/js/controllers/countersController.js
+++ app/js/controllers/countersController.js
@@ -1,10 +1,14 @@
 countersApp.controller("countersCtrl",function(){
-  // var self = this;
-  // self.count = 0;
-  // self.increment = function(){
-  //   self.count++;
-  // };
-  // self.decrement = function() {
-  //   self.count--;
-  // };
+  var self = this;
+  self.count = 0;
+  self.increment = function(){
+    self.count++;
+  };
+  self.decrement = function() {
+    self.count--;
+  };
+});
+
+countersApp.controller("FormCtrl", function($scope) {
+  $scope.typing = "";
 });
