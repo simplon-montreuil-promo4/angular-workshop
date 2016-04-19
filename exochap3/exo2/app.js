85b41af18f85eb7a0a22c7216aa6f132bbc60605
aziosmanoff <nine.aziosmanoff@gmail.com>
2016-04-19 16:11:44 +0200

    services selectionés

--- exochap3/exo2/app.js
+++ exochap3/exo2/app.js
@@ -19,8 +19,23 @@ app.controller('MainCtrl', ['$scope', function($scope) {
     active:false
 }
 ];
+$scope.totalService = 1; //par default un car 1 d'activé par default
+$scope.total = function (nouveauService){ //pour recupere 1 ou -1 c'est un parametre
+    return $scope.totalService += nouveauService;
+}
  
  $scope.toggleActive = function (service) {
     service.active = !service.active; //false ou tue
- } 
+    if (service.active === true) {
+        $scope.total(1);
+    }
+    else{
+        $scope.total(-1);
+    }
+ };
+
 }])
+//quand on clique on envoie la reference Nine
+//on le recupere dans la fonction toggle active
+//on peut acceder à l'objet savoir si c'est
+//true ou false
